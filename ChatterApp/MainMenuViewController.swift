3ce1f9bf7cf33c015ec6a0b11e16c518f3ca1446
Developer <developer@developers-imac.local>
2017-10-26 02:17:18 -0700

    Updated Info.plist with NSMicrophoneUsageDescription for microphone permissions.

--- ChatterApp/MainMenuViewController.swift
+++ ChatterApp/MainMenuViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 class MainMenuViewController: UIViewController {
     @IBOutlet weak var loadAudioProfileButton: UIButton!
     @IBOutlet weak var recordNewAudioProfileButton: UIButton!
@@ -14,6 +16,16 @@ class MainMenuViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        AVAudioSession.sharedInstance().requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                // Microphone allowed, do what you like!
+                
+            } else {
+                // User denied microphone. Tell them off!
+                
+            }
+        }
         self.title = "Main Menu"
     }
 }
